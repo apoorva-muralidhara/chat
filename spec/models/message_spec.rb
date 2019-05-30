622d8fe772dd68e7a5f85ffe24adb614366dfeb6
Apoorva Muralidhara <apoorva_muralidhara@yahoo.com>
2019-05-30 03:29:47 -0400

    Added tests for Message's belongs_to associations.

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Message do
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:room) }
+  end
 end
