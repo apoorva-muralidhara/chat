fceb2c3b9f45001ae7100920edafb3ca3bab6294
Apoorva Muralidhara <apoorva_muralidhara@yahoo.com>
2019-05-30 03:27:20 -0400

    Added Message model and table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_30_065959) do
+ActiveRecord::Schema.define(version: 2019_05_30_072431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 2019_05_30_065959) do
     t.datetime "updated_at", null: false
     t.index ["room_id"], name: "index_memberships_on_room_id"
     t.index ["user_id"], name: "index_memberships_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "room_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "rooms", force: :cascade do |t|
@@ -39,4 +49,6 @@ ActiveRecord::Schema.define(version: 2019_05_30_065959) do
 
   add_foreign_key "memberships", "rooms"
   add_foreign_key "memberships", "users"
+  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
 end
