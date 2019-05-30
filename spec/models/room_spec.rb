36ac39522f6772cd6da34916ab4658b7d0e2036f
Apoorva Muralidhara <apoorva_muralidhara@yahoo.com>
2019-05-30 02:59:16 -0400

    Added presence and uniqueness validations for room name.

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe Room, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Room do
+  let(:name) { 'action_cable_enthusiasts' }
+
+  subject(:room) { create(:room, name: name) }
+
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_uniqueness_of(:name) }
 end
